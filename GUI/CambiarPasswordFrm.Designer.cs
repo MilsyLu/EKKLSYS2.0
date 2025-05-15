ab26b16e1d072dd89d7d87d4e7fabcafd19c78cb
Cristian David Cardenas Murillo <cardenasmurillocristiandavid@gmail.com>
2025-05-15 16:55:46 -0500

    Diseño de FormCamabiocontraseña modificado

--- GUI/CambiarPasswordFrm.Designer.cs
+++ GUI/CambiarPasswordFrm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class CambiarPasswordFrm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,12 +15,9 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.textPasswordActual = new System.Windows.Forms.TextBox();
             this.textNuevaPassword = new System.Windows.Forms.TextBox();
@@ -36,95 +26,113 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.brnCambiarPassword = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(37)))), ((int)(((byte)(84)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 64);
+            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Lavender;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(129, 61);
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 32);
+            this.label1.Size = new System.Drawing.Size(236, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Cambiar contraseña";
+            this.label1.Text = "CAMBIAR CONTRASEÑA";
             // 
             // textPasswordActual
             // 
-            this.textPasswordActual.Location = new System.Drawing.Point(132, 173);
+            this.textPasswordActual.BackColor = System.Drawing.Color.White;
+            this.textPasswordActual.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.textPasswordActual.Location = new System.Drawing.Point(26, 133);
             this.textPasswordActual.Name = "textPasswordActual";
-            this.textPasswordActual.Size = new System.Drawing.Size(331, 22);
+            this.textPasswordActual.Size = new System.Drawing.Size(331, 26);
             this.textPasswordActual.TabIndex = 1;
             this.textPasswordActual.UseSystemPasswordChar = true;
-            this.textPasswordActual.TextChanged += new System.EventHandler(this.textCambioPassword_TextChanged);
             // 
             // textNuevaPassword
             // 
-            this.textNuevaPassword.Location = new System.Drawing.Point(132, 251);
+            this.textNuevaPassword.BackColor = System.Drawing.Color.White;
+            this.textNuevaPassword.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.textNuevaPassword.Location = new System.Drawing.Point(26, 213);
             this.textNuevaPassword.Name = "textNuevaPassword";
-            this.textNuevaPassword.Size = new System.Drawing.Size(331, 22);
+            this.textNuevaPassword.Size = new System.Drawing.Size(331, 26);
             this.textNuevaPassword.TabIndex = 2;
             this.textNuevaPassword.UseSystemPasswordChar = true;
-            this.textNuevaPassword.TextChanged += new System.EventHandler(this.textCambioPassword1_TextChanged);
             // 
             // textConfirmarPassword
             // 
-            this.textConfirmarPassword.Location = new System.Drawing.Point(132, 331);
+            this.textConfirmarPassword.BackColor = System.Drawing.Color.White;
+            this.textConfirmarPassword.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.textConfirmarPassword.Location = new System.Drawing.Point(26, 293);
             this.textConfirmarPassword.Name = "textConfirmarPassword";
-            this.textConfirmarPassword.Size = new System.Drawing.Size(331, 22);
+            this.textConfirmarPassword.Size = new System.Drawing.Size(331, 26);
             this.textConfirmarPassword.TabIndex = 3;
             this.textConfirmarPassword.UseSystemPasswordChar = true;
-            this.textConfirmarPassword.TextChanged += new System.EventHandler(this.textCambioPassword2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Lavender;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(132, 142);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 18);
+            this.label2.Size = new System.Drawing.Size(148, 18);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Contraseña actual";
+            this.label2.Text = "Contraseña actual:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Lavender;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(132, 218);
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(23, 183);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 18);
+            this.label3.Size = new System.Drawing.Size(147, 18);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Nueva contraseña";
+            this.label3.Text = "Nueva contraseña:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Lavender;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(132, 300);
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(23, 263);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(195, 18);
+            this.label4.Size = new System.Drawing.Size(222, 18);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Confirmar nueva contraseña";
+            this.label4.Text = "Confirmar nueva contraseña:";
             // 
             // brnCambiarPassword
             // 
-            this.brnCambiarPassword.Location = new System.Drawing.Point(132, 399);
+            this.brnCambiarPassword.BackColor = System.Drawing.Color.DarkOrchid;
+            this.brnCambiarPassword.FlatAppearance.BorderSize = 0;
+            this.brnCambiarPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brnCambiarPassword.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.brnCambiarPassword.ForeColor = System.Drawing.Color.White;
+            this.brnCambiarPassword.Location = new System.Drawing.Point(242, 350);
             this.brnCambiarPassword.Name = "brnCambiarPassword";
             this.brnCambiarPassword.Size = new System.Drawing.Size(164, 38);
             this.brnCambiarPassword.TabIndex = 7;
             this.brnCambiarPassword.Text = "Confirmar";
-            this.brnCambiarPassword.UseVisualStyleBackColor = true;
-            this.brnCambiarPassword.Click += new System.EventHandler(this.brnCambiarPassword_Click);
+            this.brnCambiarPassword.UseVisualStyleBackColor = false;
             // 
             // CambiarPasswordFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Indigo;
-            this.ClientSize = new System.Drawing.Size(800, 522);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(37)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(600, 400);
             this.Controls.Add(this.brnCambiarPassword);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -132,10 +140,11 @@
             this.Controls.Add(this.textConfirmarPassword);
             this.Controls.Add(this.textNuevaPassword);
             this.Controls.Add(this.textPasswordActual);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "CambiarPasswordFrm";
-            this.Text = "CambiarPasswordFrm";
-            this.Load += new System.EventHandler(this.CambiarPasswordFrm_Load);
+            this.Text = "Cambiar Contraseña";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,6 +152,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textPasswordActual;
         private System.Windows.Forms.TextBox textNuevaPassword;
